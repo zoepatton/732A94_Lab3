f5f833a107f1ac11d460b9731ad39cfef1917d6b
Zoe Patton <zoepa801@student.liu.se>
2020-09-15 11:22:47 +0200

    adding dataset

---
+++ data.R
@@ -0,0 +1,12 @@
+#' Dataset for Dijkstra function
+#' 
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{v1}{Starting node.}
+#'  \item{v2}{Ending node.}
+#'  \item{w}{Weight/distance from v1 node to v2 node.}
+#' }
+#' @source Wikipedia
+#'   \url{https://en.wikipedia.org/wiki/Dijkstra%27s algorithm} 
+#'   
+"wiki_graph"
